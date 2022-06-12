8c6d5e4c289fcd04a481c6e3e24c98f0629cf661
June Choi <junny7020@gmail.com>
2022-06-12 16:36:19 +0100

    Update 2024.py

--- 20220605/2024.py
+++ 20220605/2024.py
@@ -10,13 +10,14 @@ for i in range(4):
 
 move = int(input())
 
-# if(move == 0):
-#     # move left
+if(move == 0):
+    # move left
+    print(puzzle[0])
 # elif(move == 1):
-#     # move up
+#     move up
 # elif(move == 2):
-#     # move right
+#     move right
 # elif(move == 3):
-#     # move down
+#     move down
 
 print(puzzle)
